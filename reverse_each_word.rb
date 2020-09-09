87d86caf9ea937223365a48d04abb15aa17e128b
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-09 21:34:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_sentence = []
   array = string.split
-  array.each do |word|
+  array.collect do |word|
     new_sentence << word.reverse
   end
   return new_sentence.join(" ")
